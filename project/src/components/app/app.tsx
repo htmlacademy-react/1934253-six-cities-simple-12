b7ed01480582f6ab2f852863e739c679db42546a
DenisDenis1991 <i.denisdenis1991@gmail.com>
2023-03-10 13:46:42 +0300

    create page componients

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPages from '../../pages/main-page/main-page';
+
+type MainPageProps = {
+  countRooms: number;
+}
+
+
+function App({countRooms}: MainPageProps): JSX.Element {
+  return <MainPages countRooms={countRooms}/>;
 }
 
 export default App;
