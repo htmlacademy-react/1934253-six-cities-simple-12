c89f8d878ba5f79febc2b348ebffbf8fb61d94d7
DenisDenis1991 <93926317+DenisDenis1991@users.noreply.github.com>
2023-03-23 17:33:24 +0300

    Добавил типы для моков

---
+++ project/src/types/types.ts
@@ -0,0 +1,37 @@
+type Person = {
+  id: number;
+  avatarUrl: string;
+  isPro: boolean;
+  name: string;
+}
+
+type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+type City = {
+  location: Location;
+  name: string;
+}
+
+type OfferCard = {
+  id: number;
+  isPremium: boolean;
+  previewImage: string;
+  images: string[]
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+  bedrooms: number;
+  maxAdults: number;
+  goods: string[];
+  description: string;
+  city: City;
+  location: Location;
+  host: Person;
+}
+
+export type OfferCards = OfferCard[];
